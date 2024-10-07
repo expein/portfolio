2fdff6145813815c1df5be624587f5063a0aef9c
expein <santyalzateo05@gmail.com>
2024-10-07 14:17:40 -0500

    change imge profile

--- src/components/SectionToMe/components/ProfileImage.tsx
+++ src/components/SectionToMe/components/ProfileImage.tsx
@@ -4,7 +4,7 @@ import myimg from "@/assets/img/My.jpg";
 const ProfileImage = () => (
   <div className="flex items-center justify-center">
     <div className="rounded-full overflow-hidden lg:w-[400px] lg:h-[400px] sm:w-[300px] sm:h-[300px] w-[200px] h-[200px] md:mt-0 mt-10 relative" style={{ filter: 'drop-shadow(0 4px 6px rgba(255, 0, 0, 0.3))' }}>
-      <Image src={myimg} alt="sectionToMe" layout="fill" className="object-cover" />
+      <Image src={myimg} alt="sectionToMe" layout="fill" className="object-cover" style={{ objectPosition: '75% 25%' }} />
     </div>
   </div>
 );
