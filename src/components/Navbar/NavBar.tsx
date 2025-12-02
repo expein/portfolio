b6533bed25886b9cd63030a9b13281da5e7fbf76
Santiago <santyalzateo05@gmail.com>
2025-12-02 14:33:33 -0500

    refactor Navbar components to use rounded-lg styling for buttons and containers

--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -11,7 +11,7 @@ const NavBar = () => {
         <div className="fixed top-0 left-0 w-full z-50">
             <div className="w-full flex justify-center">
                 <div className="w-full max-w-[1440px] text-white flex items-center justify-center p-[15px]">
-                    <div className="w-full lg:w-1/2 flex justify-around items-center rounded-3xl p-[5px] bg-palletTwoSecondary gap-1">
+                    <div className="w-full lg:w-1/2 flex justify-around items-center rounded-lg p-[5px] bg-palletTwoSecondary gap-1">
                         <div className="hidden sm:flex justify-around items-center w-full gap-1">
                             {
                                 NavbarData.map((item, index) => (
@@ -28,7 +28,7 @@ const NavBar = () => {
                             }
                         </div>
                         <div className="sm:hidden flex flex-col w-full">
-                            <button onClick={() => setIsDropdownOpen(!isDropdownOpen)} className="text-white bg-palletTwoPrimary rounded-full h-8 sm:h-9 md:h-10">
+                            <button onClick={() => setIsDropdownOpen(!isDropdownOpen)} className="text-white bg-palletTwoPrimary rounded-lg h-8 sm:h-9 md:h-10">
                                 Menu
                             </button>
                             <div className={`flex flex-col items-center w-full bg-palletTwoSecondary rounded-lg gap-1 transition-all duration-300 ${isDropdownOpen ? 'max-h-screen opacity-100 mt-2' : 'max-h-0 opacity-0 overflow-hidden'}`} style={{ visibility: isDropdownOpen ? 'visible' : 'hidden' }}>
--- src/components/Navbar/components/OptionsNavbar.tsx
+++ src/components/Navbar/components/OptionsNavbar.tsx
@@ -9,7 +9,7 @@ interface OptionNavbarProps {
 const OptionNavbar: React.FC<OptionNavbarProps> = ({ name, href, onClick }) => {
     return (
         <div className="flex items-center justify-center w-full">
-            <a href={href} onClick={onClick} className="relative inline-flex items-center justify-center w-full text-white transition-all duration-200 bg-palletTwoPrimary rounded-full h-8 sm:h-9 md:h-10 hover:bg-palletTwoTertiary">
+            <a href={href} onClick={onClick} className="relative inline-flex items-center justify-center w-full text-white transition-all duration-200 bg-palletTwoPrimary rounded-lg h-8 sm:h-9 md:h-10 hover:bg-palletTwoTertiary">
                 {name}
             </a>
         </div>
